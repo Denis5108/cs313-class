24497d749f7b22627ab068a66f5cadd52a782d48
Denis Garley <gar18056@byui.edu>
2020-09-26 14:11:34 -0600

    commit changes

--- web/week02/main.js
+++ web/week02/main.js
@@ -10,7 +10,7 @@ function addSlides(n) {
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("slides");
-    if (n > slide.length) {slideIndex = 1}
+    if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
