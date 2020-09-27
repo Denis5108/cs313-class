edd5475bae230a61c3431e2f7103a9af324b7b20
Denis Garley <gar18056@byui.edu>
2020-09-26 23:05:07 -0600

    commit added

---
+++ web/week02/main.js
@@ -0,0 +1,14 @@
+
+function myFunction() {
+    var x, text;
+
+    x = document.getElementById("num").value;
+
+    if (isNaN(x) || x !== 20)
+    {
+        text = "Invalid age";
+    } else {
+        text = "Age is Valid";
+    }
+    document.getElementById("demo").innerHTML = text;
+}
