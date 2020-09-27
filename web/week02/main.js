41956c832845f1e496e7977d9de69e6fd68d00b4
Denis Garley <gar18056@byui.edu>
2020-09-26 21:16:30 -0600

    commit changes

--- web/week02/main.js
+++ web/week02/main.js
@@ -1,19 +1,4 @@
 
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function addSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
+function click() {
+    document.getElementById("list").innerHTML += " Yes";
 }
