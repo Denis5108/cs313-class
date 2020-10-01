d6353a144064801e63c7432759a947599c35c0dd
Denis Garley <gar18056@byui.edu>
2020-10-01 12:42:49 -0600

    added index.php

--- web/week03/index.php
+++ web/week03/index.php
@@ -5,9 +5,10 @@
         // add a list to put into a list of Orders in a shopping cart.
         // I want the user to be able to add items in a shopping list and view the items
         ?>
-        <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>"
+        <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
             Item: <input type="text" name="item">
             <input type="submit">
+        </form>
 
         <?php
             echo "<h1> You put a " . $_POST["item"] . " in your shopping list</h1>";
