f936b9fd0eefe63c17fab6156086af9cdc338316
Denis Garley <gar18056@byui.edu>
2020-10-01 13:47:21 -0600

    added index.php

--- web/week03/index.php
+++ web/week03/index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <div>
+            <div style="background-color:">Donuts</div>
+            <div style="background-color:">Pizza</div>
+            <div style="background-color:">Chicken</div>
+            <div style="background-color:">Orange Juice</div>
+        </div>
         <?php
         // add a list to put into a list of Orders in a shopping cart.
         // I want the user to be able to add items in a shopping list and view the items
         ?>
-        <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
             Item: <input type="text" name="item">
             <input type="submit">
         </form>
