9636ef9d3a5b06decbb099fe91119ce205cec918
Denis Garley <gar18056@byui.edu>
2020-10-02 13:16:48 -0600

    added several pages

--- web/week03/shoppingCart.php
+++ web/week03/shoppingCart.php
@@ -7,7 +7,7 @@
         <nav>Our Decour Has Never Been Better</nav>
 
         <?php 
-            $items = array("Button");
+            $items = array("Button1, Buttoon2, Button3, Button4, Button5");
             forEach($items as $item)
             {
                 echo "<div class=\"items\">" . $item . "</div>";
