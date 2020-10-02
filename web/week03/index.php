2d919b8d18bf29c90791b75a2ada25a7fae4820d
Denis Garley <gar18056@byui.edu>
2020-10-01 20:11:57 -0600

    added index.php

--- web/week03/index.php
+++ web/week03/index.php
@@ -1,23 +1,32 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <div>
+         <div>
             <div style="background-color:yellow">Donuts</div>
             <div style="background-color:green">Pizza</div>
             <div style="background-color:lightblue">Chicken</div>
             <div style="background-color:orange">Orange Juice</div>
         </div>
         <?php
-        // add a list to put into a list of Orders in a shopping cart.
-        // I want the user to be able to add items in a shopping list and view the items
+        $servername = "localhost";
+        $username   = "username";
+        $password   = "password";
+
+        // create connection
+        $conn = mysqli_connect($servername, $username, $password);
+
+        // check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        echo "Connected successfully";
         ?>
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            
+        <!--<form method="post" action="<?php //echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
             Item: <input type="text" name="item">
             <input type="submit">
-        </form>
+        </form>-->
 
-        <?php
-            echo "<h1> You put a " . $_POST["item"] . " in your shopping list</h1>";
-        ?>
+        <?php //echo "<h1> You put a " . $_POST["item"] . " in your shopping list</h1>";?> -->
 </head>
     </html>
