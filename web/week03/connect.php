2048268160b0f9abf0a4714287adb9c492ab9616
Denis Garley <gar18056@byui.edu>
2020-10-02 09:09:00 -0600

    added several pages

--- web/week03/connect.php
+++ web/week03/connect.php
@@ -1,9 +1,35 @@
 <?php
 
-    $username = 'root';
-    $password =  '';
-    $database =  'testdb';
+    $servername = "localhost";
+    $username   = "username";
+    $password   = "password";
+    $database   = "testdb";
+     
+    
+    // connection
+    $conn = new mysqli($servername, $username, $password);
 
-    $databe = new mysqli('localhost', $username, $password, $database) or die("Unable to connect");
-    echo "Sucess!";  
+
+    // checking connection
+    if (!$conn) {
+        die("connection failed: " . mysqli_connect_error());
+    }
+    echo "connected successfully";
+
+    // create table
+    $sql = "CREATE TABLE Customers (
+        id INT(6) unsigned auto_increment primary key,
+        firstName varchar(30),
+        lastname  varchar(30),
+        email     varchar(30),
+        address   varchar(30)
+    )";
+
+    if (mysql_query($conn, $sql)) {
+        echo "Table Customers created";
+    } else {
+        echo "Error creating table: " . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
 ?>
