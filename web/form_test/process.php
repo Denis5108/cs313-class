deb6b945ff8eb59d5cd5c77b7559c5e7785e37c7
Denis Garley <gar18056@byui.edu>
2020-10-03 10:43:59 -0600

    added commit

--- web/form_test/process.php
+++ web/form_test/process.php
@@ -3,7 +3,7 @@
    $servername = "localhost";
    $username = "root";
    $password = "";
-   $dbname = testdb"";
+   $dbname = "testdb";
    
    // Create connection
    $conn = mysqli_connect($servername, $username, $password, $dbname);
