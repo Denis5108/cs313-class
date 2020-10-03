a50af749a5e742aab03493dadd074b0d0cf51418
Denis Garley <gar18056@byui.edu>
2020-10-03 14:22:35 -0600

    added

--- web/week03/shoppingCart.php
+++ web/week03/shoppingCart.php
@@ -11,7 +11,7 @@
             $password   = "";
             $dbname     = "testdb";
 
-            $conn = mysqli_connect($servername, $username, $password);
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
 
             if (!conn) {
                 die("Connection failed: " . mysqli_connect_error());
