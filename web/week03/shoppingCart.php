fe5f14eb5ff721f6685178b83f0af932d0ddb9c6
Denis Garley <gar18056@byui.edu>
2020-10-03 13:33:24 -0600

    added

--- web/week03/shoppingCart.php
+++ web/week03/shoppingCart.php
@@ -5,15 +5,8 @@
         </head>
     <body>
         <nav>Our Decour Has Never Been Better</nav>
-        <?php 
-            $items = array("Button1, Buttoon2, Button3, Button4, Button5");
-
-
-            foreach($items as $item)
-            {
-                echo "<div class=\"items\">" . $item . "</div>";
-                echo "<br>";
-            }
+        <?php
+            $conn = new mysqli("localhost", "root", "", "testdb");
         ?>
     </body>
     </html>
