e9b93c15b50d513de22e7556bb9fcec28c7848a2
Denis Garley <gar18056@byui.edu>
2020-10-03 16:18:39 -0600

    added commit

--- web/week03/shoppingCart.php
+++ web/week03/shoppingCart.php
@@ -7,8 +7,18 @@
 	    <link rel="stylesheet" type="text/css" href="styler.css">
         </head>
     <body>
-        <nav>Our Decour Has Never Been Better</nav>
-        <?php
-?>
+    <div class="">
+     <form method="post" action="checkout.php">
+            <div class="products">
+                <img src="unknown" />
+                <h4 class="price">Price</h4>
+                <input type="text" name="quantity" class="form"  />
+                <input type="hidden" name="name" value="name"/>
+                <input type="hidden" name="price" value="price" />
+                <input type="submit" name="addtocart" class="btn btn-info" value="Add to cart" />
+            </div>
+        </form>
+
+    </div>
     </body>
     </html>
