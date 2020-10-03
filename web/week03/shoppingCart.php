3876ec2df1ac1793822e9337dd50c5f77dc888b6
Denis Garley <gar18056@byui.edu>
2020-10-03 13:41:18 -0600

    added

--- web/week03/shoppingCart.php
+++ web/week03/shoppingCart.php
@@ -6,7 +6,17 @@
     <body>
         <nav>Our Decour Has Never Been Better</nav>
         <?php
-            $conn = new mysqli("localhost", "root", "", "testdb");
+            $servename = "localhost";
+            $username  = "root";
+            $password  = "";
+
+            $conn = new mysqli($servername, $username, $password, "testdb");
+
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+            } else {
+                echo "connected succefully";
+            }
         ?>
     </body>
     </html>
