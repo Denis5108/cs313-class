deedd38da52bf53b68baaffe8c05d6a5f9197678
Denis Garley <gar18056@byui.edu>
2020-10-03 15:56:16 -0600

    added commit

--- web/week03/shoppingCart.php
+++ web/week03/shoppingCart.php
@@ -1,36 +1,14 @@
+<?php
+    include 'connect.php';
+?>
 <!DOCTYPE html>
 <html>
     <head>
-	    <link rel="stylesheet" type="text/css" href="styler.css?v=<?php echo time(); ?>">
+	    <link rel="stylesheet" type="text/css" href="styler.css">
         </head>
     <body>
         <nav>Our Decour Has Never Been Better</nav>
         <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "testdb";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-
-$sql = "SELECT id, firstname, lastname FROM MyGuests WHERE lastname='Doe'";
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-  // output data of each row
-  while($row = mysqli_fetch_assoc($result)) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-  }
-} else {
-  echo "0 results";
-}
-
-mysqli_close($conn);
 ?>
     </body>
     </html>
