3d7bef86992f8e3bc011eac0fbc87fad58d8f7d8
Denis Garley <gar18056@byui.edu>
2020-10-03 07:48:37 -0600

    added test page

---
+++ web/form_test/process.php
@@ -0,0 +1,26 @@
+<?php
+    // Get values passe from form in login.php file
+    $username = $POST['username'];
+    $password = $POST['password'];
+
+    // to prevent mysql injection
+    $username = stripcslashes($username);
+    $password = stripcslashes($password);
+    $username = mysql_real_escape_string($username);
+    $password = myswl_real_escape_string($password);
+
+    // connect to the server and select database
+    mysql_connect("localhost", "root", "");
+    mysql_select_db("login");
+
+    // Query the database for user
+    $result = mysql_query("selecy * from where username = '$username' and password = '$password'")
+    or die("Failed to query database" . mysql_erro());
+
+    $row = mysql_fetch_array($result);
+    if ($row['username'] == $username && $row['password'] == $password) {
+        echo "Logign sucess!!! Welcome " . $row['username';]
+    } else {
+        echo "Failes to login!";
+    }
+?>
