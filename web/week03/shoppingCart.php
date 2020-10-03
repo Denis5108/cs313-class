ced64e4b3eae35e9ea65b37fbff0e2a41077b6a3
Denis Garley <gar18056@byui.edu>
2020-10-03 14:20:56 -0600

    added

--- web/week03/shoppingCart.php
+++ web/week03/shoppingCart.php
@@ -9,13 +9,22 @@
             $servername = "localhost";
             $username   = "root";
             $password   = "";
+            $dbname     = "testdb";
 
             $conn = mysqli_connect($servername, $username, $password);
 
             if (!conn) {
                 die("Connection failed: " . mysqli_connect_error());
-            } else {
-                echo "Connected succesfully";
+            }
+
+            $sql = "SELECT customerID FROM customers";
+            $result = mysqli_query($conn, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+                // output data of each row
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "id: " . $row["customerID"];
+                }
             }
         ?>
     </body>
