41d4448f5b287a3d28b9d9ff7d1fafc490ae3c3a
Denis Garley <gar18056@byui.edu>
2020-10-03 07:55:38 -0600

    added test page

--- web/form_test/process.php
+++ web/form_test/process.php
@@ -7,7 +7,7 @@
     $username = stripcslashes($username);
     $password = stripcslashes($password);
     $username = mysql_real_escape_string($username);
-    $password = myswl_real_escape_string($password);
+    $password = mysql_real_escape_string($password);
 
     // connect to the server and select database
     mysql_connect("localhost", "root", "");
@@ -15,9 +15,9 @@
 
     // Query the database for user
     $result = mysql_query("selecy * from where username = '$username' and password = '$password'")
-    or die("Failed to query database" . mysql_erro());
+    or die("Failed to query database" . mysql_error());
 
-    $row = mysql_fetch_array($result);
+    $row = mysqli_fetch_array($result);
     if ($row['username'] == $username && $row['password'] == $password) {
         echo "Logign sucess!!! Welcome " . $row['username';]
     } else {
