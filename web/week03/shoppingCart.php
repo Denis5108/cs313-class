892f6c58f5af86da6ca48998c0de39fe8a92df9f
Denis Garley <gar18056@byui.edu>
2020-10-03 15:54:01 -0600

    added stuff

--- web/week03/shoppingCart.php
+++ web/week03/shoppingCart.php
@@ -6,26 +6,31 @@
     <body>
         <nav>Our Decour Has Never Been Better</nav>
         <?php
-            $servername = "localhost";
-            $username   = "root";
-            $password   = "";
-            $dbname     = "testdb";
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "testdb";
 
-            $conn = mysqli_connect($servername, $username, $password, $dbname);
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
 
-            if (!$conn) {
-                die("Connection failed: " . mysqli_connect_error());
-            }
+$sql = "SELECT id, firstname, lastname FROM MyGuests WHERE lastname='Doe'";
+$result = mysqli_query($conn, $sql);
 
-            // $sql = "SELECT customerID FROM users";
-            // $result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
 
-            // if (mysqli_num_rows($result) > 0) {
-            //     // output data of each row
-            //     while ($row = mysqli_fetch_assoc($result)) {
-            //         echo "id: " . $row["customerID"];
-            //     }
-            // }
-        ?>
+mysqli_close($conn);
+?>
     </body>
     </html>
