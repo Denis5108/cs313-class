e34aad3b46b45e2f7fabe37566671d335172e708
Denis Garley <gar18056@byui.edu>
2020-10-03 14:29:21 -0600

    added

--- web/week03/shoppingCart.php
+++ web/week03/shoppingCart.php
@@ -13,19 +13,19 @@
 
             $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-            if (!conn) {
+            if (!$conn) {
                 die("Connection failed: " . mysqli_connect_error());
             }
 
             $sql = "SELECT customerID FROM customers";
             $result = mysqli_query($conn, $sql) or die("Queriying data failed");
 
-            if (mysqli_num_rows($result) > 0) {
-                // output data of each row
-                while ($row = mysqli_fetch_assoc($result)) {
-                    echo "id: " . $row["customerID"];
-                }
-            }
+            // if (mysqli_num_rows($result) > 0) {
+            //     // output data of each row
+            //     while ($row = mysqli_fetch_assoc($result)) {
+            //         echo "id: " . $row["customerID"];
+            //     }
+            // }
         ?>
     </body>
     </html>
