4636b67b6abc1bdb45dd364f6399ce8e970afb09
Denis Garley <gar18056@byui.edu>
2020-10-03 08:01:33 -0600

    added test page

--- web/form_test/process.php
+++ web/form_test/process.php
@@ -6,16 +6,16 @@
     // to prevent mysql injection
     $username = stripcslashes($username);
     $password = stripcslashes($password);
-    $username = mysql_real_escape_string($username);
-    $password = mysql_real_escape_string($password);
+    $username = mysqli_real_escape_string($username);
+    $password = mysqli_real_escape_string($password);
 
     // connect to the server and select database
-    mysql_connect("localhost", "root", "");
-    mysql_select_db("login");
+    mysqli_connect("localhost", "root", "");
+    mysqli_select_db("login");
 
     // Query the database for user
-    $result = mysql_query("selecy * from where username = '$username' and password = '$password'")
-    or die("Failed to query database" . mysql_error());
+    $result = mysqli_query("selecy * from where username = '$username' and password = '$password'")
+    or die("Failed to query database" . mysqli_connect_error());
 
     $row = mysqli_fetch_array($result);
     if ($row['username'] == $username && $row['password'] == $password) {
