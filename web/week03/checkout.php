607325c4c972473d56f92aeed5d009f0a7747576
Denis Garley <gar18056@byui.edu>
2020-10-03 20:48:46 -0600

    added commit

--- web/week03/checkout.php
+++ web/week03/checkout.php
@@ -3,13 +3,9 @@
 session_start();
 $_SESSION['test'] = 'test';
 ?>
-<!DOCTYPE html>
-<html>
-  <body>
-      <?php 
-        if (isset($_POST['submit']) && !empty($_POST['submit'])) {
-            header("Location: index.php");
-        }
-      ?>
-</body>
-    </html>
+
+<?php 
+if (isset($_POST['submit']) && !empty($_POST['submit'])) {
+    header('Location: index.php');
+}
+?>
