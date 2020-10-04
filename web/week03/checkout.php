cf401e6f43e7c1c48b94aaa1db2ba0d7bd6d042b
Denis Garley <gar18056@byui.edu>
2020-10-03 20:50:16 -0600

    added commit

--- web/week03/checkout.php
+++ web/week03/checkout.php
@@ -6,6 +6,6 @@ $_SESSION['test'] = 'test';
 
 <?php 
 if (isset($_POST['submit']) && !empty($_POST['submit'])) {
-    header('Location: index.php');
+    header('Location: shoppingCart.php');
 }
 ?>
